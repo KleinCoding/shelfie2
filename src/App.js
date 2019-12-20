1a520cd1e99f7bdfca974d7c12fba233ce570887
KleinCoding <kleincoding@gmail.com>
2019-12-20 16:37:36 -0600

    Functional component finished. All but routing completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,66 @@
 import React, { Component } from "react";
-import { HashRouter, Link } from "react-router-dom";
 import Dashboard from "./Components/Dashboard/Dashboard";
 import Header from "./Components/Header/Header";
 import Form from "./Components/Form/Form";
 import axios from "axios";
 import "./App.css";
 
+
+
+//////////////////////////////////ROUTING BELOW COMMENT OUT IF BROKEN
+//////////////ROUTING IMPORTS
+// import { HashRouter, Link, Switch } from "react-router-dom";
+// import Routes from "./routes"
+
+// function App() {
+//   return (
+//     // <div>
+//     // <Header />
+//     // </div>
+//     <HashRouter>
+//       <header>
+//         <ul>
+//           <li>
+//             <Link to="/">Dashboard</Link>
+//           </li>
+//           <li>
+//             <Link to="/about">About</Link>
+//           </li>
+//           <li>
+//             <Link to="/about/contact">Contact</Link>
+//             </li>
+//             <li>
+//               <Link to ="/Blah">404 View</Link>
+            
+//           </li>
+//           <li><Link to ="/store">Store</Link></li>
+//         </ul>
+//       </header>
+//       <main>
+//         <div className="App">
+//           {/* <Switch>
+//             <Route component={Contact}exact path="/about/contact" />
+//             <Route component={About} exact path="/about" />
+//             <Route component={Home} exact path="/" />
+//             <Route render= {() => <h1> 404 Not Found! DOOOOOOOOOOM! </h1>} />
+
+//           </Switch> */} 
+//               {Routes}
+//         </div>
+//       </main>
+//     </HashRouter>
+//   );
+// }
+
+// export default App;
+
+
+
+//////////////////////////ROUTING ABOVE COMMENT OUT IF BROKEN
+
+
+
+////////////////////////ORIGINAL CODE BELOW UNCOMMENT IF ROUTING BREAKS
 class App extends Component {
   constructor() {
     super();
@@ -24,42 +79,6 @@ class App extends Component {
 
   render() {
     console.log(this.state.products);
-
-//////////////////////////////////ROUTING BELOW COMMENT OUT IF BROKEN
-//     <HashRouter>
-//  <Switch>
-//     <header>
-//       <ul>
-//         <li>
-//           <Link to="/">Home</Link>
-//         </li>
-//         <li>
-//           <Link to="/about">About</Link>
-//         </li>
-//         <li>
-//           <Link to="/about/contact">Contact</Link>
-//           </li>
-//           <li>
-//             <Link to ="/Blah">404 View</Link>
-          
-//         </li>
-//         <li><Link to ="/store">Store</Link></li>
-//       </ul>
-//     </header>
-//     <main>
-
-//             {Routes}
-    
-//     </main>
-//     </Switch>
-//   </HashRouter>
-
-//////////////////////////ROUTING ABOVE COMMENT OUT IF BROKEN
-
-
-
-////////////////////////ORIGINAL CODE BELOW UNCOMMENT IF ROUTING BREAKS
-
     return ( 
     <div className="App">
     <div>
@@ -77,8 +96,8 @@ class App extends Component {
 
       </div>
     );
-    //////////////////////////////////////////////ORIGINAL CODE ABOVE UNCOMMENT IF ROUTING BREAKS
-  }
-}
-
+    
+}}
+//////////////////////////////////////////////ORIGINAL CODE ABOVE UNCOMMENT IF ROUTING BREAKS
+  
 export default App;
